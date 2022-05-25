2c56e4252fe57178bc65f3d9275aabfba19f0927
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-25 09:27:21 +0600

    add user review section

--- src/pages/Dashboard/AddReview/AddReview.js
+++ src/pages/Dashboard/AddReview/AddReview.js
@@ -2,8 +2,46 @@ import React from "react";
 
 const AddReview = () => {
   return (
-    <div>
-      <h2>Add Your Review</h2>
+    <div className="px-12">
+      <h2 className="text-3xl text-indigo-400 my-2">Add Your Review</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Your Name"
+          class="input input-bordered w-full max-w-lg"
+        />
+        <textarea
+          class="textarea textarea-bordered w-full max-w-lg mt-3"
+          placeholder="Your Comment"
+        ></textarea>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-lg">
+        <input
+          type="text"
+          name="email"
+          placeholder="Your Email"
+          class="block input input-bordered w-full max-w-sm"
+        />
+        <input
+          type="text"
+          placeholder="your ratings not more than 5 "
+          class="block input input-bordered w-full max-w-sm"
+        />
+        <input
+          type="text"
+          placeholder="Your Job"
+          class="block input input-bordered max-w-sm"
+        />
+        <input
+          type="file"
+          name="image"
+          id=""
+          class="block input input-bordered max-w-sm"
+        />
+      </div>
+      <button className="btn btn-primary my-2 w-full max-w-lg mx-auto">
+        Submit
+      </button>
     </div>
   );
 };
--- src/pages/Dashboard/MyOrders/MyOrders.js
+++ src/pages/Dashboard/MyOrders/MyOrders.js
@@ -1,15 +1,51 @@
 import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../Firebase.init";
 
 const MyOrders = () => {
   const [orders, setOrders] = useState([]);
+  const [user] = useAuthState(auth);
 
   useEffect(() => {
-    fetch();
-  }, []);
+    fetch(`http://localhost:5000/order`)
+      .then((res) => res.json())
+      .then((data) => {
+        setOrders(data);
+        console.log(data);
+      });
+  }, [user]);
 
   return (
     <div>
-      <h2>My Orders </h2>
+      <h2>My Orders {orders.length} </h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Orders Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* <!-- row 1 --> */}
+            {orders.map((order, index) => (
+              <tr>
+                <th>{index + 1}</th>
+                <td>{order.name}</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+                <td className="mr-2">
+                  <button className="btn btn-xs mr-4">Pay</button>
+                  <button className="btn btn-xs ">Cancel</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };

d11eeff74cb439071b9e8fc1d0227a04899d6cf0
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-05-26 19:52:32 +0600

    added payment system

--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../../shared/Loading/Loading";
+import CheckoutForm from "../CheckoutForm/CheckoutForm";
+
+// stripe publishable key
+const stripePromise = loadStripe(
+  "pk_test_51L1YKcCSwG8CgGDROMBz3kz6xC2QCemcho77guUezPDTNqlO8hsjNKlkoyAGBBuDfA2IONO3yDTvfLYYLzIBKau300ZZAJ4ySB"
+);
 
 const Payment = () => {
   const { id } = useParams();
@@ -34,9 +42,9 @@ const Payment = () => {
         </div>
         <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
           <div class="card-body">
-            {/* <Elements stripe={stripePromise}>
+            <Elements stripe={stripePromise}>
               <CheckoutForm order={order} />
-            </Elements> */}
+            </Elements>
           </div>
         </div>
       </div>

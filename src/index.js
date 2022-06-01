f062e056c6b135333917af43bb6ba3cb65330d6e
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-06-02 01:34:59 +0600

    added all tols for manageO

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import axios from "axios"
+
 import { QueryClient, QueryClientProvider } from "react-query";
 
+axios.defaults.baseURL="http://localhost:5000"
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
